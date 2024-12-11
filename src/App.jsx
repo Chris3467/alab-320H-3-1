9c9ee9abb368eda56ea0acfb6c10a3d5a6cfda1f
Chris3467 <chris.collado815@gmail.com>
2024-12-10 23:00:15 -0500

    finished

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Learner from "./components/Learner";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>ALAB320H.3.1</h1>
+        <Learner />
+      </div>
+    </>
+  );
+}
+
+export default App;
